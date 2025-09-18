ed03fc05ecfa98bd0f650c3329903553338c1f95
henriqueapc16 <henriqueapc1671@outlook.com>
2025-09-18 17:23:10 -0300

    aula python senac

---
+++ codicional.py
@@ -0,0 +1,32 @@
+"""""
+# Mostra uma mensagem
+print(" hello wolrd")
+#testando
+
+documentação
+"""
+#digite os três numeros
+num1= int(input("digite o primeiro numero:"))
+num2= int(input("digite o segundo numero:"))
+num3= int(input("digite o terceiro numero:"))
+# condicional de maior
+maior = num1
+if num2 > maior:
+    maior = num2
+if num3 > maior:
+    maior = num3
+# codicional de menor
+menor = num1
+if num2 < menor:
+    menor = num2
+if num3 < menor:
+    menor = num3
+# definição de media
+media = (num1+num2+num3)/3
+#exibir na tela
+print("\n resultado:")
+print("\n o maior numero é : ",maior)
+print("\n o menor numero é : ", menor)
+print("\n a media é : ", media)
+
+#conclução
