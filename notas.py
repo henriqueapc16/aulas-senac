681ceddf89ca44238e8bdc64708e170360b065b5
henriqueapc16 <henriqueapc1671@outlook.com>
2025-09-24 17:22:43 -0300

    viagem

---
+++ notas.py
@@ -0,0 +1,11 @@
+# media aritmétrica das notas
+nota1 = int(input("digite sua nota:"))
+nota2 = int(input("digite sua nota:"))
+ 
+media =( nota1 + nota2 ) /2
+if media >= 7:
+    print( "você foi aprovado",media)
+else:
+    print( "você foi reprovado" )
+
+
---
+++ viagem.py
@@ -0,0 +1,17 @@
+
+valor = float(input("digite o valor disponivel para viagem (r$):"))
+regiao = str(input("digite a cidade de origem:"))
+
+
+destinos = ["Recife (PE)","Porto de Galinha(PE)","RIO de Janeiro","Gramado"]
+
+for i in (destinos):
+    print("opçoes de destinos disponiveis:")
+    primeiro_destino = destinos [0]
+    print(f"{1:primeiro_destino}")
+    segundo_destino = destinos [1]
+    print(f"{2:primeiro_destino}")
+    terceiro_destino = destinos [2]
+    print(f"{3:primeiro_destino}")
+    quarto_destino = destinos [3]
+    print(f"{4:primeiro_destino}")
