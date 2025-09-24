1a340a5691c4905aff9d72e931dd4345c53cbf0d
henriqueapc16 <henriqueapc1671@outlook.com>
2025-09-24 15:04:19 -0300

    três novo desafiu

---
+++ loop_for.py
@@ -0,0 +1,22 @@
+# for repete até a condição ser atendida ou seja (vai imprimir de zero a cinco).
+"""for numero in range (2,11):
+ print("número",numero)
+"""
+# concluido 
+# 🔽 houver um equivoco achei que era pra chamar o input com while: true, sendo que era uma .
+"""while True:
+    numero = int(input("digite um número:"))
+    for soma in range (11):
+        
+    print("número",soma)
+
+    soma +=numero"""
+
+soma = 0 # variavel soa
+for i in range (11): # (for) laço de repetição 
+    numero = int(input(f"digite  um número:"))
+    soma += numero # variavel soma recebe resultado de numero que é atribuido da direita a esqueda .
+
+print("total",soma) # imprimir 
+
+#concluido
