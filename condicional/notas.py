b4e53b8c62e925982d7bd52fcc9e5afb5421bcad
henriqueapc16 <henriqueapc1671@outlook.com>
2025-09-26 16:51:28 -0300

    concluido controle  fila

--- condicional/notas.py
+++ condicional/notas.py
@@ -4,8 +4,8 @@ nota2 = int(input("digite sua nota:"))
  
 media =( nota1 + nota2 ) /2
 if media >= 7:
-    print( "você foi aprovado",media)
+    print(f"você foi aprovado,[media]")
 else:
-    print( "você foi reprovado" )
+    print(f"você foi reprovado[media]" )
 
 
--- controle_de _fila.py
+++ controle_de _fila.py
@@ -2,11 +2,37 @@
 crie um algritmo para organizar a entrada de pessoa em um evento 
 evintando  tumulto e garantindo a checagem de ingresso ."""
 
-ingressos = 100
+print("controle de fila do moto: \n")
+ # variavel que recebe o valor digitado pelo usuário .
+ingresso = input("Digite o código do seu ingresso: \n")
+# ingressos validos disponivel 
+ingressos_valido = ["001","002","003","004","005"]
 
-for ingresso in range(ingressos):
+ingressos_oculpados =[ "006","007","008","009","0010"]
+# condicional, pega a variavel digitada pelo o usuário e compara com á variavel ingresso_valido . 
+if ingresso in ingressos_valido:
+# se for válido iprimir na tela.
 
-    if ingresso != -1:
+        print("ingresso válido")
 
-        print("ingresso")
+elif ingresso in ingressos_oculpados :
+        
+        print(" ingresso já está oculpado")
 
+else:
+         # se não  imprimar na tela .
+         print("ingresso não é válido")
+
+"""
+print("contrle de fila: \n")
+
+checar_ingresso = input ("Digite o código do ingresso: \n")
+
+ingressos_validos = ("1","2","3","4","5")
+
+match  checar_ingresso in ingressos_validos:
+  
+  case ("1","2","3","4","5"):
+    print("ingresso válidado ")
+  case _:
+    print("ingresso invalido") """
