6eee4a05bd31e6e87cb19ef98399917bcdcc5933
henriqueapc16 <henriqueapc1671@outlook.com>
2025-10-07 22:50:43 -0300

    concluido

--- aulas-senac/Condicionais/requeridade.py
+++ aulas-senac/Condicionais/requeridade.py
@@ -1,11 +1,12 @@
 from typing import Final #"Essa variável/atributo é final (constante), não pode ser sobrescrita." importa class final para informa o fim da constante 
 IDADE_MININA: Final = 18 
-idade= int(input("digite sua idade"))
-if idade >=18: 
-         print("você é menor de idade")
-elif idade >=18 and 60: 
-        print(" você é maior de idade")
+idade= int(input("digite sua idade: \n"))
+if idade >= 60: 
+         print(" você chegou na terceira idade ")
+
+elif idade >= 18: 
+        print("você é maior de idade ")
 else:   
-       print (" voçê chegou na terceira idade ")
+       print ("você é menor de idade")
 
     # falta concluir 
